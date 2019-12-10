16ee6437d3023bbe38f529b3d340b6ea573d08af
Bas Nijholt <basnijholt@gmail.com>
2019-12-10 20:54:54 +0100

    get correct key

--- json2yaml.py
+++ json2yaml.py
@@ -10,7 +10,7 @@ p.add_argument("yaml_fname", nargs="?", type=str, default=sys.stdin)
 
 json_fname = p.parse_args().json_fname
 with open(json_fname) as f:
-    info = json.load(f)
+    info = json.load(f)["data"]["config"]
 
 yaml_fname = p.parse_args().yaml_fname
 with open(yaml_fname, "w") as f:
