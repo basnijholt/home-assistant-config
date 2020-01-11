ee8776e9db7bb23e2d51f5cbf4751827f133b1ac
Bas Nijholt <basnijholt@gmail.com>
2020-01-11 18:10:27 +0100

    'update www/community/lovelace-xiaomi-vacuum-map-card'

--- www/community/lovelace-xiaomi-vacuum-map-card/texts.js
+++ www/community/lovelace-xiaomi-vacuum-map-card/texts.js
@@ -16,15 +16,6 @@ const texts = {
         repeats: "Times:",
         confirmation: "Command sent!"
     },
-    "es": {
-        mode: "Modos",
-        goToTarget: "Ir al objetivo",
-        zonedCleanup: "Zona de limpieza",
-        zones: "Zonas",
-        run: "Empezar",
-        repeats: "Veces:",
-        confirmation: "Comando enviado!"
-    },
     "de": {
         mode: "Modus",
         goToTarget: "Gehe zum Zielpunkt",
@@ -42,6 +33,24 @@ const texts = {
         run: "Start",
         repeats: "Gentagelser:",
         confirmation: "Kommando afsendt!"
+    },
+    "es": {
+        mode: "Modos",
+        goToTarget: "Ir al objetivo",
+        zonedCleanup: "Zona de limpieza",
+        zones: "Zonas",
+        run: "Empezar",
+        repeats: "Veces:",
+        confirmation: "Comando enviado!"
+    },
+    "fr": {
+        mode: "Mode",
+        goToTarget: "Aller sur la cible",
+        zonedCleanup: "Nettoyage de zone",
+        zones: "Zones",
+        run: "Démarrage",
+        repeats: "Répéter:",
+        confirmation: "Commande envoyée!"
     },
     "it": {
         mode: "Modalità",
@@ -78,6 +87,15 @@ const texts = {
         run: "Uruchom",
         repeats: "Razy:",
         confirmation: "Komenda wysłana!"
+    },
+    "ru": {
+        mode: "Режим",
+        goToTarget: "Движение к цели",
+        zonedCleanup: "Зональная уборка",
+        zones: "Зоны",
+        run: "Старт",
+        repeats: "Повторы:",
+        confirmation: "Команда отправлена"
     },
     "se": {
         mode: "Läge",
