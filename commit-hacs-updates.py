079cd56f2d2a625c71ac04c9220faff285fe9398
Bas Nijholt <basnijholt@gmail.com>
2020-01-12 16:49:55 +0100

    improve commit script

--- commit-hacs-updates.py
+++ commit-hacs-updates.py
@@ -5,13 +5,12 @@ p = subprocess.run(cmd, capture_output=True).stdout.decode()
 
 folders_to_add = set()
 for line in p.split("\n"):
-    if not line.startswith(" M"):
-        continue
-    path = line[3:]
-    community = "www/community/"
-    if path.startswith(community):
-        plugin = path.split("/")[2]
-        folders_to_add.add(community + plugin)
+    if line.startswith(" M") or line.startswith("??"):
+        path = line[3:]
+        community = "www/community/"
+        if path.startswith(community):
+            plugin = path.split("/")[2]
+            folders_to_add.add(community + plugin)
 
 for folder in folders_to_add:
     print(folder)
