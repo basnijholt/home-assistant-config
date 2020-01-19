d7d22aacfa84c1e1ccff4f7c341e6864fd4933d8
Bas Nijholt <basnijholt@gmail.com>
2020-01-20 00:26:20 +0100

    put rhasspy files in Git

---
+++ rhasspy/init.sh
@@ -0,0 +1,6 @@
+
+ln -s /config/rhasspy/sentences.ini /share/rhasspy/profiles/en/sentences.ini
+ln -s /config/rhasspy/custom_words.txt /share/rhasspy/profiles/en/custom_words.txt
+ln -s /config/rhasspy/slots/lights /share/rhasspy/profiles/en/slots/lights
+ln -s /config/rhasspy/slots/media_player /share/rhasspy/profiles/en/slots/media_player
+ln -s /config/rhasspy/slots/media_players /share/rhasspy/profiles/en/slots/media_players
