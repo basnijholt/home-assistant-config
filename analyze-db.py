c916112c70e746ad94180451f1dfd934fb25e008
Bas Nijholt <basnijholt@gmail.com>
2020-01-30 00:30:53 +0100

    update db-analysis.py

--- analyze-db.py
+++ analyze-db.py
@@ -2,23 +2,39 @@ import json
 import sqlite3
 from collections import Counter, defaultdict
 
-cnt = defaultdict(int)
 
 conn = sqlite3.connect("home-assistant_v2.db")
 cur = conn.cursor()
+
+
+def analyze(d):
+    N = 50
+    cnt = Counter(d)
+    most_common = cnt.most_common(N)
+    total = sum(cnt.values())
+
+    print(f"The {N} most common entries are:\n")
+    for k, v in most_common:
+        print(f"{k}: {v/total * 100}%, ({v})")
+
+
+events = defaultdict(int)
 cur.execute("SELECT * FROM events")
 
 while row := cur.fetchone():
     i, which, dict_str, *_ = row
     info = json.loads(dict_str)
     if "entity_id" in info:
-        cnt[info["entity_id"]] += 1
+        events[info["entity_id"]] += len(dict_str)
 
-N = 50
-cnt = Counter(cnt)
-most_common = cnt.most_common(N)
-total = sum(cnt.values())
+print("Statistics on 'events' table.")
+analyze(events)
 
-print(f"The {N} most common entries are:\n")
-for k, v in most_common:
-    print(f"{k}: {v/total * 100}%, ({v})")
+states = defaultdict(int)
+cur.execute("SELECT * FROM states")
+
+while row := cur.fetchone():
+    i, domain, entity_id, state, attributes, *_ = row
+    states[entity_id] += len(state) + len(attributes)
+print("\nStatistics on 'states' table.")
+analyze(states)
