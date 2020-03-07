8a8d228555787d0dab78513c1d0ec28bd2e4edfb
Bas Nijholt <basnijholt@gmail.com>
2020-03-07 11:45:11 +0100

    update commit-hacs-updates.py

--- commit-hacs-updates.py
+++ commit-hacs-updates.py
@@ -1,12 +1,17 @@
 import subprocess
+from pathlib import Path
 
 cmd = "git status --porcelain".split()
 p = subprocess.run(cmd, capture_output=True).stdout.decode()
+
+ha_version = ".HA_VERSION"
+ha_update = False
 
 folders_to_add = set()
 for line in p.split("\n"):
     if line.startswith(" M") or line.startswith("??"):
         path = line[3:]
+
         community = "www/community/"
         if path.startswith(community):
             plugin = path.split("/")[2]
@@ -17,7 +22,20 @@ for line in p.split("\n"):
             theme = path.split("/")[1]
             folders_to_add.add(themes + theme)
 
+        hacs = "custom_components/hacs/"
+        if path.startswith(hacs):
+            folders_to_add.add(hacs)
+        
+        if path == ha_version:
+            ha_update = True
+
 for folder in folders_to_add:
     print(folder)
     subprocess.run(f"git add {folder}".split())
     subprocess.run(["git", "commit", "-m", f"update {folder} via HACS"])
+
+if ha_update:
+    with open(ha_version) as f:
+        version = f.read()
+    subprocess.run(f"git add {ha_version}".split())
+    subprocess.run(["git", "commit", "-m", f"update Home Assistant to {version}"])
