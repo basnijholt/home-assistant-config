52dd3b48ce6e5e9d13fb9e64c588f4232099a3b9
root <root@homeassistant.local.hass.io>
2020-10-22 20:45:00 +0200

    update www/community/lovelace-battery-entity-row via HACS

--- www/community/lovelace-battery-entity-row/battery-entity-row.js
+++ www/community/lovelace-battery-entity-row/battery-entity-row.js
@@ -1,6 +1,6 @@
 ((LitElement) => {
     console.info(
-        '%c BATTERY-ENTITY-ROW %c 1.0.0 ',
+        '%c BATTERY-ENTITY-ROW %c 1.1.0 ',
         'color: cyan; background: black; font-weight: bold;',
         'color: darkblue; background: white; font-weight: bold;',
     );
@@ -64,7 +64,9 @@
                     ${this.renderSecondaryInfo()}
                 </div>
                 <div class="state">
-                    ${this.state.level}&nbsp;${this.state.unit}
+                    ${this.state.valid
+                ? html`${this.state.level}${this.state.unit && html`&nbsp;${this.state.unit}`}`
+                : this._hass.localize('state.default.unknown')}
                 </div>
             </div>` : html`
             <hui-warning>
@@ -93,13 +95,15 @@
                 this.stateObj = this._config.entity in hass.states ? hass.states[this._config.entity] : null;
 
                 if (this.stateObj) {
+                    const charging = this.getChargingState(this._config.charging)
                     const batteryLevel = this.getBatteryLevel(this._config.attribute);
 
                     this.state = {
-                        name: this._config.name || this.stateObj.attributes.friendly_name,
+                        valid: batteryLevel !== null,
                         level: batteryLevel,
-                        unit: this._config.unit || '%',
-                        icon: this._config.icon || this.getIcon(batteryLevel),
+                        name: this._config.name || this.stateObj.attributes.friendly_name,
+                        unit: this._config.unit === false ? null : (this._config.unit || '%'),
+                        icon: this._config.icon || this.getIcon(batteryLevel, charging),
                         color: this.getColor(batteryLevel)
                     };
                 }
@@ -111,19 +115,31 @@
             if (this.stateObj.attributes.battery) batteryValue = this.stateObj.attributes.battery;
             if (this.stateObj.attributes.battery_level) batteryValue = this.stateObj.attributes.battery_level;
             if (this.stateObj.attributes[attribute]) batteryValue = this.stateObj.attributes[attribute];
-            return Number.isFinite(parseInt(batteryValue)) ? Math.round(parseInt(batteryValue, 10)) : 0;
+            return Number.isFinite(parseInt(batteryValue)) ? Math.round(parseInt(batteryValue, 10)) : null;
         }
 
-        getIcon(batteryLevel) {
+        getChargingState(chargingConfig) {
+            if (!chargingConfig) return false;
+            if (chargingConfig === true) return this.stateObj.state === 'on';
+
+            const entity = (chargingConfig.entity && chargingConfig.entity in this._hass.states)
+                ? this._hass.states[chargingConfig.entity] : this.stateObj;
+            const state = chargingConfig.attribute ? entity.attributes[chargingConfig.attribute] : entity.state;
+            return state === 'on';
+        }
+
+        getIcon(batteryLevel, charging) {
+            if (!batteryLevel) return 'mdi:battery-unknown';
             const roundedLevel = Math.round(batteryLevel / 10) * 10;
             return roundedLevel >= 100
-                ? 'mdi:battery'
+                ? (charging ? 'mdi:battery-charging-100' : 'mdi:battery')
                 : roundedLevel === 0
-                    ? 'mdi:battery-outline'
-                    : `mdi:battery-${roundedLevel}`;
+                    ? (charging ? 'mdi:battery-charging-outline' : 'mdi:battery-outline')
+                    : (charging ? 'mdi:battery-charging-' : 'mdi:battery-') + roundedLevel;
         }
 
         getColor(batteryLevel) {
+            if (!batteryLevel) return 'unknown';
             const warning = this._config.warning || 35;
             const critical = this._config.critical || 15;
             return (batteryLevel > warning)
