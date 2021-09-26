a7d9c15e1080d1d8d2dd4128d5a525077e42b6ef
Bas Nijholt <basnijholt@gmail.com>
2021-09-26 23:15:24 +0200

    update KEF

--- custom_components/kef_custom/number.py
+++ custom_components/kef_custom/number.py
@@ -7,7 +7,7 @@ from aiokef.aiokef import DSP_OPTION_MAPPING
 
 from homeassistant.components.media_player import DOMAIN as MEDIA_PLAYER_DOMAIN
 from homeassistant.components.number import DOMAIN as NUMBER_DOMAIN, NumberEntity
-from homeassistant.const import CONF_HOST, DEVICE_DEFAULT_NAME
+from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
@@ -34,24 +34,29 @@ async def async_setup_platform(
     numbers = data.setdefault(NUMBER_DOMAIN, {})
     speaker = data[MEDIA_PLAYER_DOMAIN]
 
-    for name, dsp_attr, options in (
-        ["Desk dB", "desk_db", DSP_OPTION_MAPPING["desk_db"]],
-        ["Wall dB", "wall_db", DSP_OPTION_MAPPING["wall_db"]],
-        ["Treble dB", "treble_db", DSP_OPTION_MAPPING["treble_db"]],
-        ["High Hz", "high_hz", DSP_OPTION_MAPPING["high_hz"]],
-        ["Low Hz", "low_hz", DSP_OPTION_MAPPING["low_hz"]],
-        ["Sub dB", "sub_db", DSP_OPTION_MAPPING["sub_db"]],
+    if speaker.dsp is None:
+        await speaker.update_dsp()
+
+    for dsp_attr, options in (
+        ["desk_db", DSP_OPTION_MAPPING["desk_db"]],
+        ["wall_db", DSP_OPTION_MAPPING["wall_db"]],
+        ["treble_db", DSP_OPTION_MAPPING["treble_db"]],
+        ["high_hz", DSP_OPTION_MAPPING["high_hz"]],
+        ["low_hz", DSP_OPTION_MAPPING["low_hz"]],
+        ["sub_db", DSP_OPTION_MAPPING["sub_db"]],
     ):
+        name = dsp_attr.replace("_", " ")
         min_value = min(options)
         max_value = max(options)
         step = options[1] - options[0]
         select = KefDSPNumber(
-            unique_id=f"{speaker._unique_id}_{dsp_attr}",
+            unique_id=f"{speaker.unique_id}_{dsp_attr}",
             name=f"{speaker.name} {name}",
             icon="mdi:equalizer",
             min_value=min_value,
             max_value=max_value,
             step=step,
+            value=speaker.dsp[dsp_attr],  # type: ignore
             speaker=speaker,
             dsp_attr=dsp_attr,
         )
@@ -73,6 +78,7 @@ class KefDSPNumber(NumberEntity):
         min_value: float,
         max_value: float,
         step: float,
+        value: float,
         speaker: KefMediaPlayer,
         dsp_attr: str,
     ) -> None:
@@ -80,11 +86,12 @@ class KefDSPNumber(NumberEntity):
         self._speaker = speaker
         self._dsp_attr = dsp_attr
         self._attr_unique_id = unique_id
-        self._attr_name = name or DEVICE_DEFAULT_NAME
+        self._attr_name = name
 
         self._attr_max_value = max_value
         self._attr_min_value = min_value
         self._attr_step = step
+        self._attr_value = value
 
         self._attr_icon = icon
         self._attr_device_info = {
@@ -92,13 +99,18 @@ class KefDSPNumber(NumberEntity):
             "name": name,
         }
 
-    async def async_set_value(self, value: str) -> None:
+    async def async_set_value(self, value: float) -> None:
         """Update the current selected value."""
         _LOGGER.debug("Setting %s to %s", self._attr_name, value)
-        await getattr(self._speaker, f"set_{self._dsp_attr}")(value)
+        self._attr_value = value
+        if value != "Unknown":  # type: ignore
+            await getattr(self._speaker, f"set_{self._dsp_attr}")(value)
+            self._attr_available = True
+        else:
+            self._attr_available = False
         self.async_write_ha_state()
 
     async def async_update(self, **kwargs):
         """Update the select entity with the latest DSP settings."""
-        self._attr_value = self._speaker._dsp[self._dsp_attr]
+        self._attr_value = self._speaker.dsp[self._dsp_attr]
         self.async_write_ha_state()
--- custom_components/kef_custom/select.py
+++ custom_components/kef_custom/select.py
@@ -2,10 +2,11 @@
 from __future__ import annotations
 
 import logging
+from typing import Any, cast
 
 from homeassistant.components.media_player import DOMAIN as MEDIA_PLAYER_DOMAIN
 from homeassistant.components.select import DOMAIN as SELECT_DOMAIN, SelectEntity
-from homeassistant.const import CONF_HOST, DEVICE_DEFAULT_NAME
+from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
@@ -16,7 +17,7 @@ from .media_player import KefMediaPlayer
 _LOGGER = logging.getLogger(__name__)
 
 
-def str_to_option(option):
+def str_to_option(option: str) -> Any:
     """Parse the option."""
     if option == "off":
         return False
@@ -25,7 +26,7 @@ def str_to_option(option):
     return option
 
 
-def option_to_str(option):
+def option_to_str(option) -> str:
     """Parse the option."""
     if option is False:
         return "off"
@@ -50,24 +51,26 @@ async def async_setup_platform(
     selects = data.setdefault(SELECT_DOMAIN, {})
     speaker = data[MEDIA_PLAYER_DOMAIN]
 
-    if speaker._dsp is None:
+    if speaker.dsp is None:
         await speaker.update_dsp()
 
-    for name, dsp_attr, options in (
-        ["Bass Extension", "bass_extension", ["Standard", "Less", "Extra"]],
-        ["Sub Polarity", "sub_polarity", ["-", "+"]],
-        ["Desk Mode", "desk_mode", ["on", "off"]],
-        ["Wall Mode", "wall_mode", ["on", "off"]],
-        ["Phase Correction", "phase_correction", ["on", "off"]],
-        ["High Pass", "high_pass", ["on", "off"]],
+    for dsp_attr, options in (
+        ["bass_extension", ["Standard", "Less", "Extra"]],
+        ["sub_polarity", ["-", "+"]],
+        ["desk_mode", ["on", "off"]],
+        ["wall_mode", ["on", "off"]],
+        ["phase_correction", ["on", "off"]],
+        ["high_pass", ["on", "off"]],
     ):
-        current_option = option_to_str(speaker._dsp[dsp_attr])
+        dsp_attr = cast(str, dsp_attr)
+        name = dsp_attr.replace("_", " ")
+        current_option = option_to_str(speaker.dsp[dsp_attr])  # type: ignore
         select = MediaSelect(
-            unique_id=f"{speaker._unique_id}_{dsp_attr}",
+            unique_id=f"{speaker.unique_id}_{dsp_attr}",
             name=f"{speaker.name} {name}",
             icon="mdi:equalizer",
             current_option=current_option,
-            options=options,
+            options=cast(list[str], options),  # pylint: disable=unsubscriptable-object
             speaker=speaker,
             dsp_attr=dsp_attr,
         )
@@ -95,7 +98,7 @@ class MediaSelect(SelectEntity):
         self._speaker = speaker
         self._dsp_attr = dsp_attr
         self._attr_unique_id = unique_id
-        self._attr_name = name or DEVICE_DEFAULT_NAME
+        self._attr_name = name
         self._attr_current_option = current_option
         self._attr_icon = icon
         self._attr_options = options
@@ -108,12 +111,17 @@ class MediaSelect(SelectEntity):
         """Update the current selected option."""
         self._attr_current_option = option
         option = str_to_option(option)
-        _LOGGER.debug("Setting %s to %s (%s)", self._attr_name, option, self._attr_current_option)
+        _LOGGER.debug(
+            "Setting %s to %s (%s)", self._attr_name, option, self._attr_current_option
+        )
         if option != "Unknown":
+            self._attr_available = True
             await self._speaker.set_mode(**{self._dsp_attr: option})
+        else:
+            self._attr_available = False
         self.async_write_ha_state()
 
     async def async_update(self, **kwargs):
         """Update the select entity with the latest DSP settings."""
-        self._attr_current_option = option_to_str(self._speaker._dsp[self._dsp_attr])
+        self._attr_current_option = option_to_str(self._speaker.dsp[self._dsp_attr])
         self.async_write_ha_state()
