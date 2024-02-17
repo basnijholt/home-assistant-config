b99b1010120048c68a9a5c97d73153577ed1c1c6
root <root@homeassistant.local.hass.io>
2024-02-17 11:45:03 -0800

    update www/community/lovelace-template-entity-row via HACS

--- www/community/lovelace-template-entity-row/rollup.config.js
+++ www/community/lovelace-template-entity-row/rollup.config.js
@@ -1,8 +1,8 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
 import typescript from "rollup-plugin-typescript2";
-import terser from "@rollup/plugin-terser";
-import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
+import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 
 const dev = process.env.ROLLUP_WATCH;
 
@@ -16,9 +16,7 @@ export default {
     nodeResolve(),
     json(),
     typescript(),
-    babel({
-      exclude: "node_modules/**",
-    }),
+    getBabelOutputPlugin({ presets: ["@babel/preset-env"] }),
     !dev && terser({ format: { comments: false } }),
   ],
 };
