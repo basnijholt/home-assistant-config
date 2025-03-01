7143a221b80bb93eb897d5b787d7a366a9ca7da4
root <root@homeassistant.local.hass.io>
2025-02-28 20:00:00 -0800

    update www/community/advanced-camera-card via HACS

--- www/community/advanced-camera-card/advanced-camera-card.js
+++ www/community/advanced-camera-card/advanced-camera-card.js
@@ -1,1 +1,1 @@
-import"./card-1ab50869.js";
+import"./card-dfbafb45.js";
---
+++ www/community/advanced-camera-card/timeline-2c91b860.js
@@ -0,0 +1,13 @@
+import{_ as e,n as t,t as i,a,x as r,r as o,d4 as n}from"./card-dfbafb45.js";import"./timeline-core-f90bfadc.js";import"./startOfHour-15de2c0d.js";import"./endOfDay-7cf755fe.js";import"./date-picker-50a7ec85.js";let s=class extends a{render(){return this.timelineConfig?r`
+      <advanced-camera-card-timeline-core
+        .hass=${this.hass}
+        .viewManagerEpoch=${this.viewManagerEpoch}
+        .timelineConfig=${this.timelineConfig}
+        .thumbnailConfig=${this.timelineConfig.controls.thumbnails}
+        .cameraManager=${this.cameraManager}
+        .cameraIDs=${this.cameraManager?.getStore().getCameraIDsWithCapability({anyCapabilities:["clips","snapshots","recordings"]})}
+        .cardWideConfig=${this.cardWideConfig}
+        .itemClickAction=${"none"===this.timelineConfig.controls.thumbnails.mode?"play":"select"}
+      >
+      </advanced-camera-card-timeline-core>
+    `:r``}static get styles(){return o(n)}};e([t({attribute:!1})],s.prototype,"hass",void 0),e([t({attribute:!1})],s.prototype,"viewManagerEpoch",void 0),e([t({attribute:!1})],s.prototype,"timelineConfig",void 0),e([t({attribute:!1})],s.prototype,"cameraManager",void 0),e([t({attribute:!1})],s.prototype,"cardWideConfig",void 0),s=e([i("advanced-camera-card-timeline")],s);export{s as AdvancedCameraCardTimeline};
